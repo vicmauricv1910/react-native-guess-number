fbee6293a1c8f1d7bf321aa457ec247937a134de
Victor Mauricio Cota Vidaurre <mauricio.cota@jalasoft.com>
2021-12-12 11:59:11 -0400

    FEATURE - Adding header

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.screen}>
+      <Header title={"Guess a number"} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
