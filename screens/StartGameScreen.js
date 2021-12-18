71bf8a0870605fcdcbc12414d238947b350e8577
mauricio cota <mauricio.cota@jalasoft.com>
2021-12-18 00:18:44 -0400

    Adding alert error message in startGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 
 import Card from "../components/Card";
@@ -28,7 +29,12 @@ const StartGameScreen = (props) => {
 
   const confirmInputHandler = () => {
     const choosenNumber = parseInt(enteredValue);
-    if (choosenNumber === NaN || choosenNumber <= 0 || choosenNumber > 99) {
+    if (isNaN(choosenNumber) || choosenNumber <= 0 || choosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
       return;
     }
     setConfirmed(true);
